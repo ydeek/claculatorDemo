478b89a6218972cf4c14be283bd1e5cedaece0a4
Yazan Deek <ydeek91@yahoo.com>
2022-04-26 12:23:40 -0700

    added new files

---
+++ calculatorTest/src/main/java/CalculatorTest/calculatorTest/Calculator.java
@@ -0,0 +1,13 @@
+package CalculatorTest.calculatorTest;
+
+public interface Calculator {
+	
+	 private int ad d(int a, int b);
+	 int substract(int a, int b);
+     int multiply(int a, int b);
+	 int devide(int a, int b);
+	 int modulo(int a, int b);
+
+	 public int calculate(String query);
+
+}
---
+++ calculatorTest/src/test/java/CalculatorTest/calculatorTest/CalculatorTests.java
@@ -0,0 +1,36 @@
+package CalculatorTest.calculatorTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTests {
+	
+	Calculator calc;
+	
+	@Test
+	public void testAdd() {
+		
+		assertEquals(5, calc.add(2,3));
+	}
+	@Test
+	public void testAdd() {
+		
+		assertEquals(5, calc.add(2,3));
+	}
+	@Test
+	public void testAdd() {
+		
+		assertEquals(5, calc.add(2,3));
+	}
+	@Test
+	public void testAdd() {
+		
+		assertEquals(5, calc.add(2,3));
+	}
+	@Test
+	public void testAdd() {
+		
+		assertEquals(5, calc.add(2,3));
+	}
+}
